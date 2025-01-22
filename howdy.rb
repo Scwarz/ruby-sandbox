b4551ddb0f7a11366133a2962ee13beb29118be7
Anatoli Kamyshev <36002768+Scwarz@users.noreply.github.com>
2025-01-22 22:17:59 +0000

    practising 1

---
+++ howdy.rb
@@ -0,0 +1,16 @@
+require "active_support/all"
+my_string = "Hey guys"
+pp my_string
+#require "./goodbye.rb"
+#pp 1.ordinalize    # => "1st"
+#pp 2.ordinalize    # => "2nd"
+#53.ordinalize   # => "53rd"
+#2009.ordinalize # => "2009th"
+#-21.ordinalize  # => "-21st"
+#-134.ordinalize # => "-134th"
+
+#pp "What's your name?"
+
+#their_name = gets.chomp
+
+#puts "Hello, " + their_name + "!"
